0f65bc3c1e6c13b0cb5fb397f3ce13d0c00cf766
Moudisa Jan <37015810+Moudisa@users.noreply.github.com>
2024-02-22 17:03:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+      <Navbar/>
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+        
+      
+    </>
+      
+    
+  );
+}
+
+export default App;
